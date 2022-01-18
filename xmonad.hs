7536af2d6c2d2b095cd3c5f08cc7a7ae7f4eec83
Andreas Niederl <82451+aniederl@users.noreply.github.com>
2022-01-18 19:14:58 +0100

    adapt for changes in xmonad 0.17

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 
+{-# OPTIONS_GHC -Wno-deprecations #-}
 -- {-# OPTIONS_GHC -cpp #-}
 -- {-
 -- #include <X11/XF86keysym.h>
@@ -13,7 +14,7 @@ import qualified XMonad.StackSet as W
 import qualified XMonad.Actions.FlexibleManipulate as Flex
 
 import XMonad.Actions.Promote
-import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWS hiding (Not)
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.SpawnOn
@@ -28,9 +29,12 @@ import XMonad.Actions.Warp
 
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
-import XMonad.Hooks.DynamicLog as DL
+--import XMonad.Hooks.DynamicLog as DL
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP as SBPP
+import XMonad.Hooks.WorkspaceHistory
 
 import XMonad.Hooks.EwmhDesktops
 
@@ -49,7 +53,7 @@ import XMonad.Layout.Reflect
 import XMonad.Layout.ComboP
 import XMonad.Layout.TwoPane
 import XMonad.Layout.Tabbed
-import XMonad.Layout.IM
+import XMonad.Layout.IM hiding (Not)
 import XMonad.Layout.Grid
 import XMonad.Layout.TrackFloating
 import XMonad.Layout.MagicFocus
@@ -61,8 +65,10 @@ import XMonad.Prompt.AppendFile
 import XMonad.Prompt.Workspace
 
 import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run
-import XMonad.Util.Scratchpad
+import XMonad.Util.WorkspaceCompare
+import qualified XMonad.Util.Hacks as Hacks
 
 import Data.Ratio
 import Data.List
@@ -126,16 +132,16 @@ myNoteXPConfig = myXPConfig
 myGSConfig = XMonad.Actions.GridSelect.def
     { gs_font = myFont }
 
+myFilter = filterOutWs [scratchpadWorkspaceTag]
 myConfig = withUrgencyHookC NoUrgencyHook urgencyConfig { suppressWhen = Focused }
-         $ docks $ XMonad.def
+         $ docks $ addEwmhWorkspaceSort (pure myFilter) $ ewmhFullscreen $ ewmh $ Hacks.javaHack XMonad.def
          { borderWidth        = 2
          , terminal           = myTerminal
          , normalBorderColor  = "#333333"
          , focusedBorderColor = "#0000ff"
-         , startupHook        = ewmhDesktopsStartup <+> docksStartupHook <+> setWMName "LG3D" <+> addEWMHFullscreen
-         , handleEventHook    = ewmhDesktopsEventHook <+> fullscreenEventHook <+> docksEventHook
-         , logHook            = ewmhDesktopsLogHook
-                              >> updatePointer (1.0, 1.0) (1, 1) -- (Relative 1.0 1.0)
+         , startupHook        = setWMName "LG3D" <+> addEWMHFullscreen
+         , handleEventHook    = handleEventHook def <+> Hacks.windowedFullscreenFixEventHook
+         , logHook            = updatePointer (1.0, 1.0) (1, 1) -- (Relative 1.0 1.0)
          , manageHook         = manageSpawn <+> myManageHook
          }
          `removeKeysP` delKeys
@@ -176,11 +182,11 @@ addEWMHFullscreen = do
 data TopicType = Code
                | Other
     deriving Eq
-data TopicItem = TopicItem { topicName   :: Topic
-                           , topicDir    :: Dir
-                           , topicAction :: X ()
-                           , topicType   :: TopicType
-                           }
+data MyTopicItem = MyTopicItem { topicName   :: Topic
+                               , topicDir    :: Dir
+                               , topicAction :: X ()
+                               , topicType   :: TopicType
+                               }
 
 
 myDefaultTopicAction = return ()
@@ -197,7 +203,7 @@ myDefaultTopicConfig = TopicConfig
 -- actions
 myActionTopics' :: [(Topic, X ())]
 myActionTopics' = [ ("admin", spawnTmuxSession "main" >>  spawnT (myTerminal ++ " -e sudo -i tmuxinator admin"))
-                  , ("music", spawn "clementine")
+                  , ("music", spawn "strawberry")
                   , ("gimp",  spawn "gimp")
                   , ("cal",   spawnT (myTerminal ++ " -e wyrd"))
                   , ("im",    spawn "skype")
@@ -222,7 +228,7 @@ myOtherTopics = [ "documents"
                 ]
 
 
-myTopics :: [TopicItem]
+myTopics :: [MyTopicItem]
 myTopics = map topicItem''    myActionTopics
            ++
            map codeTopicItem  myCodeTopics
@@ -236,14 +242,14 @@ myTopics = map topicItem''    myActionTopics
 myTopicNames :: [Topic]
 myTopicNames = map topicName myTopics
 
-topicDirMap :: [TopicItem] -> Map Topic Dir
-topicDirMap ts = M.fromList $ map (\(TopicItem n d _ _) -> (n, d)) ts
+topicDirMap :: [MyTopicItem] -> Map Topic Dir
+topicDirMap ts = M.fromList $ map (\(MyTopicItem n d _ _) -> (n, d)) ts
 
-topicActionMap :: [TopicItem] -> Map Topic (X ())
-topicActionMap ts = M.fromList $ map (\(TopicItem n _ a _) -> (n, a)) ts
+topicActionMap :: [MyTopicItem] -> Map Topic (X ())
+topicActionMap ts = M.fromList $ map (\(MyTopicItem n _ a _) -> (n, a)) ts
 
 
-updateTopicConfig :: TopicConfig -> [TopicItem] -> TopicConfig
+updateTopicConfig :: TopicConfig -> [MyTopicItem] -> TopicConfig
 updateTopicConfig tc ts = tc
     { topicDirs    = topicDirs    tc <+> (topicDirMap    ts)
     , topicActions = topicActions tc <+> (topicActionMap ts)
@@ -252,17 +258,17 @@ updateTopicConfig tc ts = tc
 myTopicConfig = updateTopicConfig myDefaultTopicConfig myTopics
 
 
-topicItem :: Topic -> Dir -> X () -> TopicType -> TopicItem
-topicItem name dir action ttype = TopicItem name topicdir action ttype
+topicItem :: Topic -> Dir -> X () -> TopicType -> MyTopicItem
+topicItem name dir action ttype = MyTopicItem name topicdir action ttype
     where topicdir =
               case dir of
                    "" -> "${HOME}/"
                    _  -> dir
 
-topicItem' :: Topic -> Dir -> X () -> TopicItem
+topicItem' :: Topic -> Dir -> X () -> MyTopicItem
 topicItem' name dir action = topicItem name dir action Other
 
-typedTopicItem :: TopicConfig -> Topic -> Dir -> String -> TopicItem
+typedTopicItem :: TopicConfig -> Topic -> Dir -> String -> MyTopicItem
 typedTopicItem tc name dir ttype | ttype == "code" = topicItem  name dir (codeTopicSession' tc name) Code
                                  | otherwise      = topicItem' name dir myDefaultTopicAction
 
@@ -276,7 +282,7 @@ typedTopicItem tc name dir ttype | ttype == "code" = topicItem  name dir (codeTo
 myTopicFile     = ".xmonad/topics"
 myCodeTopicFile = ".xmonad/code-topics"
 
-zipTopics' :: TopicConfig -> String -> String -> [TopicItem]
+zipTopics' :: TopicConfig -> String -> String -> [MyTopicItem]
 zipTopics' tc dc s = (map myZip) ( (map words) (lines s) )
     where
         myZip (x:y:z:xs) = typedTopicItem tc x y z
@@ -426,6 +432,12 @@ myLayoutHook ts = avoidStruts
         codeWS = [ topicName t | t <- ts, topicType t == Code ]
 
 
+-- Scratchpads ----------------------------------------------------------------
+
+scratchpads = [ NS "term" ((terminal myConfig) ++ " -name scratchpad -e $SHELL -c 'tmux'") (appName =? "scratchpad") (customFloating $ W.RationalRect 0.25 0.375 0.5 0.3)
+              , NS "htop" ((terminal myConfig) ++ " -name htop -e htop") (appName =? "htop") (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+              ]
+
 -- Key Bindings ----------------------------------------------------------------
 
 delKeys = ["M-" ++ [n] | n <- ['1'..'9']]
@@ -437,12 +449,12 @@ insKeys home tc =
     , ("M-d",               shiftNextScreen)
 
     -- cycle through recent workspaces
-    , ("M-h",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
-    , ("M-l",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
-    , ("M-S-h",             myShiftRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
-    , ("M-S-l",             myShiftRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
-    , ("M-<Tab>",           myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
-    , ("M-`",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-h",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-l",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-S-h",             myShiftRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-S-l",             myShiftRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-<Tab>",           myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
+    --, ("M-`",               myCycleRecentWS [xK_Alt_L, xK_Alt_R] xK_l xK_h)
 
     -- resize master pane
     , ("M-z",               sendMessage Shrink)
@@ -454,8 +466,10 @@ insKeys home tc =
     , ("M-b",               banish LowerRight)
 
     -- toggle to last workspace (like C-a C-a in screen)
-    , ("M-a",               myToggleWS)
-    , ("M-;",               myToggleWS)
+    --, ("M-a",               myToggleWS)
+    --, ("M-;",               myToggleWS)
+    , ("M-a",               myToggleTopic tc)
+    , ("M-;",               myToggleTopic tc)
 
     -- focus most recently urgent window
     , ("M-u",               focusUrgent)
@@ -493,7 +507,9 @@ insKeys home tc =
 
     -- scratchpad terminal with a screen session
     -- need to add '-name' as first argument or else urxvt won't use it
-    , ("M-s",               scratchpadSpawnActionTerminal ((terminal myConfig) ++ " -name scratchpad -e $SHELL -c 'tmux'"))
+    --, ("M-s",               scratchpadSpawnActionTerminal ((terminal myConfig) ++ " -name scratchpad -e $SHELL -c 'tmux'"))
+    , ("M-s",               namedScratchpadAction scratchpads "term")
+    , ("M-S-t",             namedScratchpadAction scratchpads "htop")
 
     --, ("M-n",               refresh)
     , ("M-C-S-q",           io (exitWith ExitSuccess))
@@ -541,10 +557,10 @@ multimediaKeys =
         --, ("<XF86AudioStop>",        spawn "xmpc stop")
         --, ("<XF86AudioPrev>",        spawn "xmpc prev")
         --, ("<XF86AudioNext>",        spawn "xmpc next")
-        , ("<XF86AudioPlay>",        spawn "qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-        , ("<XF86AudioStop>",        spawn "qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop")
-        , ("<XF86AudioPrev>",        spawn "qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-        , ("<XF86AudioNext>",        spawn "qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+        , ("<XF86AudioPlay>",        spawn "qdbus org.mpris.MediaPlayer2.strawberry /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+        , ("<XF86AudioStop>",        spawn "qdbus org.mpris.MediaPlayer2.strawberry /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop")
+        , ("<XF86AudioPrev>",        spawn "qdbus org.mpris.MediaPlayer2.strawberry /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
+        , ("<XF86AudioNext>",        spawn "qdbus org.mpris.MediaPlayer2.strawberry /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
         ]
 
 delButtons =
@@ -564,17 +580,19 @@ myGestures = M.fromList
         ]
 
 myToggleWS :: X ()
-myToggleWS = windows $ view =<< tag . head . scratchpadFilterOutWorkspace . hidden
+myToggleWS = toggleWS' [scratchpadWorkspaceTag]
+myToggleTopic :: TopicConfig -> X ()
+myToggleTopic tc = switchNthLastFocusedByScreen tc 1
 
-myCycleRecentWS = myRecentWS W.view
-myShiftRecentWS = myRecentWS shiftView'
-    where shiftView' id ws = W.view id $ W.shift id ws
-
-myRecentWS f = cycleWindowSets options
-    where
-        options w = map (f `flip` w) (recentTags w)
-        recentTags w = map tag $ tail (myWS w) ++ [head (myWS w)]
-        myWS w = scratchpadFilterOutWorkspace $ W.workspaces w
+--myCycleRecentWS = myRecentWS W.view
+--myShiftRecentWS = myRecentWS shiftView'
+--    where shiftView' id ws = W.view id $ W.shift id ws
+--
+--myRecentWS f = cycleWindowSets options
+--    where
+--        options w = map (f `flip` w) (recentTags w)
+--        recentTags w = map tag $ tail (myWS w) ++ [head (myWS w)]
+--        myWS w = scratchpadFilterOutWorkspace $ W.workspaces w
 
 
 --cycleRecentWS' = cycleWindowSets options
@@ -588,12 +606,12 @@ addTopic tc newtag = addHiddenTopic newtag >> switchTopic tc newtag
 addHiddenTopic :: String -> X ()
 addHiddenTopic newtag = addHiddenWorkspace newtag
 
--- copy to nth last focused topic
--- adapted from shiftNthLastFocused
-copyNthLastFocused :: Int -> X ()
-copyNthLastFocused n = do
-    ws <- fmap (listToMaybe . drop n) getLastFocusedTopics
-    whenJust ws $ windows . copy
+---- copy to nth last focused topic
+---- adapted from shiftNthLastFocused
+--copyNthLastFocused :: Int -> X ()
+--copyNthLastFocused n = do
+--    ws <- fmap (listToMaybe . drop n) getLastFocusedTopics
+--    whenJust ws $ windows . copy
 
 
 gridselectTopic :: TopicConfig -> GSConfig WorkspaceId -> X ()
@@ -659,9 +677,11 @@ myManageHook = composeAll $
              [ composeOne [ isDialog     -?> doCenterFloat ] ]
              ++
               -- auto float
-             [ className =? c --> doCenterFloat  | c <- myClassFloats ]
+             [ className =? c --> doCenterFloat  | c <- myClassCenterFloats ]
              ++
-             [ title     =? t --> doCenterFloat  | t <- myTitleFloats ]
+             [ title     =? t --> doCenterFloat  | t <- myTitleCenterFloats ]
+             ++
+             [ className =? c --> doFloat  | c <- myClassFloats ]
              ++
               -- ignore
              [ resource  =? r --> doIgnore | r <- myIgnores ]
@@ -676,14 +696,15 @@ myManageHook = composeAll $
               -- other hooks
              [ lowerDock
              , manageDocks
-             , scratchpadManageHookDefault
+             , namedScratchpadManageHook scratchpads
              ]
     where moveToC c w = className =? c --> doF (W.shift w)
           moveToT t w = title     =? t --> doF (W.shift t)
           floatC  c   = className =? c --> doFloat
           unfloat     = ask >>= doF . W.sink
-          myClassFloats = [ "Xmessage", "feh", "Display", "MPlayer", "mpv", "Kdiff3", "Audacious" ] --"MPlayer",
-          myTitleFloats = [ "Downloads", "Firefox Preferences", "Thunderbird Preferences", "Save As...",
+          myClassCenterFloats = [ "Xmessage", "feh", "Display", "MPlayer", "mpv", "Kdiff3", "Audacious" ] --"MPlayer",
+          myClassFloats = [ "Plasma-desktop", "plasmashell", "Klipper" ] --"MPlayer",
+          myTitleCenterFloats = [ "Downloads", "Firefox Preferences", "Thunderbird Preferences", "Save As...",
                             "Preferences...", "Confirm...", "Connect via URL", "Enter Password", "Password Required", "Transfer Files", "Rename",
                             "Make Directory", "Delete Files/Directories", "Getting directory listings", "Options", "Chmod", "Add Bookmark",
                             "Edit Bookmarks", "Delete Bookmarks", "Save Playlist", "GQview Preferences", "Inkscape Preferences (Shift+Ctrl+P)",
@@ -707,7 +728,7 @@ myManageHook = composeAll $
 
 myBitmapsDir = ".xmonad/.dzen/bitmaps/dzen"
 
-myDefaultPP = DL.def
+myDefaultPP = SBPP.def
 
 myPP :: String -> PP
 myPP home = myDefaultPP
@@ -728,7 +749,7 @@ myPP home = myDefaultPP
                     _        -> pad x
         )
     , ppTitle   = dzenColor "white" "" . dzenEscape . wrap "< " " >" -- . shorten 50
-    , ppSort    = fmap (.scratchpadFilterOutWorkspace) $ ppSort myDefaultPP
+    --, ppSort    = fmap (.scratchpadFilterOutWorkspace) $ ppSort myDefaultPP
     }
     where
         bitmapsDir = home ++ "/" ++ myBitmapsDir
@@ -744,8 +765,8 @@ onlyTitle pp = myDefaultPP { ppCurrent = const ""
                            , ppLayout  = ppLayout pp
                            , ppTitle   = ppTitle pp }
 
--- taken from XMonad.Actions.TopicSpace and modified for respecting ppSort
--- function
+---- taken from XMonad.Actions.TopicSpace and modified for respecting ppSort
+---- function
 mypprWindowSet :: TopicConfig -> PP -> X String
 mypprWindowSet tg pp = do
     winset <- gets windowset
@@ -753,14 +774,15 @@ mypprWindowSet tg pp = do
     sort' <- ppSort pp
     let empty_workspaces = map W.tag $ filter (isNothing . W.stack) $ W.workspaces winset
         maxDepth = maxTopicHistory tg
-    setLastFocusedTopic (W.tag . W.workspace . W.current $ winset)
+    setLastFocusedTopic tg
+                        (W.tag . W.workspace . W.current $ winset)
                         (`notElem` empty_workspaces)
-    lastWs <- getLastFocusedTopics
+    lastWs <- workspaceHistory
     let depth topic = fromJust $ elemIndex topic (lastWs ++ [topic])
         add_depth proj topic = proj pp . (((topic++":")++) . show) . depth $ topic
         pp' = pp { ppHidden = add_depth ppHidden, ppVisible = add_depth ppVisible }
         sortWindows = take maxDepth . sortBy (comparing $ depth . W.tag) . sort'
-    return $ DL.pprWindowSet sortWindows urgents pp' winset
+    return $ SBPP.pprWindowSet sortWindows urgents pp' winset
 
 
 myDynamicLogString :: TopicConfig -> PP -> X String
@@ -801,7 +823,7 @@ main = do
     let tc   = updateTopicConfig myTopicConfig $ fileTopicList tc tf ctf
     let conf = updateMyConfig myConfig home tc $ myTopics ++ fileTopicList tc tf ctf
     xmonad $ conf
-        { logHook            = logHook conf
+        { logHook            = logHook conf >> workspaceHistoryHookExclude [scratchpadWorkspaceTag]
                              >> (myDynamicLogWithPP tc $ (myPP home) { ppOutput = hPutStrLn din })
                              >> (myDynamicLogWithPP tc $ (myPP home) { ppOutput = hPutStrLn din2 })
         }
